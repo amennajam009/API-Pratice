691eef5fe9673225ef9b65d7e2a21d72f0cdabe2
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-03-06 18:07:22 +0500

    add

--- Controllers/ProductTestController.js
+++ Controllers/ProductTestController.js
@@ -72,11 +72,73 @@ const multiplyTwonumber = (req,res)=>{
     }
 }
 
+const AddandSub = (req,res)=>{
+    try {
+        const {number1,number2,number3}=req.body;
+        const result = number1+number2-number3;
+        res.json({
+            message:'API of add+sub three number is working',
+            Result:result,
+            Data:true
+        })
+    } catch (error) {
+        res.json({
+            message:'API of add+sub three number is not working',
+            Result:null,
+            Data:false
+        })
+    }
+}
+
+const DividingAndMulti = (req,res)=>{
+    try {
+        const {number1,number2,number3}=req.body;
+        const result = number1/number2*number3;
+
+        res.json({
+            message:'API of Divid+multi three number is working',
+            Result:result,
+            Data:true
+        })
+    } catch (error) {
+        res.json({
+            message:'API of Divid+multi three number is not working ',
+            Result:null,
+            Data:false
+        })
+    }
+}
+
+// API's Using For Loop
+
+const Printnumbers1To10 = (req,res)=>{
+  try {
+     let result = '';
+     for (i=1; i<=10;i++){
+        result+=i+'';
+     }
+     
+     res.json({
+        message:'Api of For loop is working',
+        Result:result,
+        Data:true
+     })
+  } catch (error) {
+    res.json({
+        message:'Api of For loop is not working',
+        Result:null,
+        Data:false
+    })
+  }
+}
+
 module.exports = {
     addingtwonumber,
     subtractTwonumber,
     DividingTwonumber,
-    multiplyTwonumber
-
+    multiplyTwonumber,
+    AddandSub,
+    DividingAndMulti,
+    Printnumbers1To10
     
 }
--- Routes/ProductTestRoute.js
+++ Routes/ProductTestRoute.js
@@ -2,13 +2,16 @@ const express = require('express');
 const Router =  express.Router();
 
 
-const {addingtwonumber,subtractTwonumber,DividingTwonumber,multiplyTwonumber} = require('../Controllers/ProductTestController');
+const {addingtwonumber,subtractTwonumber,DividingTwonumber,multiplyTwonumber,AddandSub,DividingAndMulti,Printnumbers1To10} = require('../Controllers/ProductTestController');
 
 
 Router.post('/addingtwonumber',addingtwonumber);
 Router.post('/subtractTwonumber' ,subtractTwonumber);
 Router.post('/DividingTwonumber' ,DividingTwonumber);
 Router.post('/multiplyTwonumber' , multiplyTwonumber);
+Router.post('/AddandSub',AddandSub);
+Router.post('/DividingAndMulti',DividingAndMulti);
+Router.post('/Printnumbers1To10',Printnumbers1To10)
 
 
 
