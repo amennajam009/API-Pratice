c91b0d1f8777f056c47d5861384a03567bff2edc
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-12 14:07:04 +0500

    add

---
+++ Controllers/devcamperApi.js
@@ -0,0 +1,30 @@
+const devcamperModel = require('../Model/devcampermodel');
+
+// Edhr mai apni Api ka format thora change kr rhe hun 
+
+
+const MyApiToPost =async (req,res) =>{
+   try {
+    const DocToSave = await devcamperModel.create(req.body);
+    res.json({
+        message:'Api Works Successfully!!',
+        Data:true,
+        Result:DocToSave
+    })
+   } catch (error) {
+     res.json({
+        message:error.message,
+        Data:false,
+        Result:null
+     })
+   }
+
+}
+
+
+
+
+
+module.exports={
+    MyApiToPost
+}
--- Routes/devcamperApi.js
+++ Routes/devcamperApi.js
@@ -2,8 +2,13 @@ const express = require('express');
 const Router = express.Router();
 
 const model = require('../Model/devcampermodel');
-const {anyapi}= require('../Controllers/devcamperApi');
+const {MyApiToPost}= require('../Controllers/devcamperApi');
 
 
+
+
+
+
+Router.post('/MyApiToPost',MyApiToPost);
 
 module.exports = Router;
