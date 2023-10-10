7e393809ebae1def877c7f2053743a834baf4e72
amennajam009 <amennajam7@gmail.com>
2023-10-10 11:16:06 +0500

    add

--- Controllers/SearchByNameApi.js
+++ Controllers/SearchByNameApi.js
@@ -4,12 +4,25 @@ const {SearchByName} = require('../Model/SearchByNameApi')
 
 const PostSearchByNameData = async(req,res) =>{
     try {
-        const {Name , country , city , address} = req.body;
-        const MappingData = await SearchByName.save({
-            Name , country , city , address
+        const {Name , country , city , phone, address} = req.body;
+        const MappingData = new SearchByName({
+            Name , country , city , phone, address
+        });
+        const DataToSave = await MappingData.save();
+        res.json({
+            message:'Api works successfully',
+            Data:true,
+            Result:DataToSave
         })
-
     } catch (error) {
-        
+        res.json({
+            message : error.message,
+            Data:false,
+            Result:null
+        })
     }
+}
+
+module.exports = {
+PostSearchByNameData
 }
